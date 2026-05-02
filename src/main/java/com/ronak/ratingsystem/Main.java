da3de1a7e8e6d736d463ecff3bd6dc981a32b82b
Ronak <sharr54@mcmaster.ca>
2026-05-02 19:01:17 -0400

    added test data in main class

--- src/main/java/com/ronak/ratingsystem/Main.java
+++ src/main/java/com/ronak/ratingsystem/Main.java
@@ -1,7 +1,30 @@
 package com.ronak.ratingsystem;
 
-public class Main {
+import com.ronak.ratingsystem.model.Episode;
+import com.ronak.ratingsystem.model.Movie;
+import com.ronak.ratingsystem.model.Season;
+import com.ronak.ratingsystem.model.TVShow;
+import com.ronak.ratingsystem.model.User;
+
+public class Main { //main class containing test data for now
     public static void main(String[] args) {
-        System.out.println("Project running 🚀");
+        User user = new User("ronak");
+
+        Movie movie = new Movie("Inception", 2010, "Sci-Fi"); //test movie object with metadata
+
+        TVShow show = new TVShow("Breaking Bad", "Crime"); //tvshow with data
+        Season season1 = new Season(1); //season with data
+
+        //2 episodes added
+        season1.addEpisode(new Episode("Pilot", 1)); 
+        season1.addEpisode(new Episode("Cat's in the Bag", 2));
+
+        //adding season1 with its two episodes
+        show.addSeason(season1);
+
+        System.out.println(user);
+        System.out.println(movie);
+        System.out.println();
+        System.out.println(show);
     }
 }
