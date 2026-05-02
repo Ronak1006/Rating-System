69737d321802cacaa8823b188458eeb55fef9de1
Ronak <sharr54@mcmaster.ca>
2026-05-02 18:18:59 -0400

    added constructor and getters for movie class logic

--- src/main/java/com/ronak/ratingsystem/model/Movie.java
+++ src/main/java/com/ronak/ratingsystem/model/Movie.java
@@ -1,5 +1,30 @@
 package com.ronak.ratingsystem.model;
 
-public class Movie {
-    
+public class Movie implements Rateable {
+    private String title;
+    private int releaseYear;
+    private String genre;
+
+    public Movie(String title, int releaseYear, String genre) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String toString() {
+        return "Movie: " + title + " (" + releaseYear + ") - " + genre;
+    }
 }
+
