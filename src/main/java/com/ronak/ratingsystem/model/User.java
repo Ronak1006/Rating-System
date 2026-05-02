1057c03b7741ed0b04ff31bcb5fbda2e1a9f7faa
Ronak <sharr54@mcmaster.ca>
2026-05-02 18:57:33 -0400

    added logic for user class

--- src/main/java/com/ronak/ratingsystem/model/User.java
+++ src/main/java/com/ronak/ratingsystem/model/User.java
@@ -1,5 +1,18 @@
 package com.ronak.ratingsystem.model;
 
-public class User {
-    
+public class User { //class for user (name and future data)
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + username;
+    }
 }
