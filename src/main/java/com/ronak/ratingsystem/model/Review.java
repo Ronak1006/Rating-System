9eb27474260444887c14d27d66ba792555b53ed3
Ronak <sharr54@mcmaster.ca>
2026-05-18 13:14:22 -0400

    add review class to model folder

--- src/main/java/com/ronak/ratingsystem/model/Review.java
+++ src/main/java/com/ronak/ratingsystem/model/Review.java
@@ -1,5 +1,38 @@
 package com.ronak.ratingsystem.model;
 
-public class Review {
-    
+import java.time.LocalDateTime; // to keep track of when review was written
+
+public class Review { //class for leaving a written review
+    private User user;
+    private Rateable target;
+    private String comment;
+    private LocalDateTime createdAt; 
+
+    public Review(User user, Rateable target, String comment) {
+        this.user = user;
+        this.target = target;
+        this.comment = comment;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Rateable getTarget() {
+        return target;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return user.getUsername() + " reviewed " + target.getTitle() + ": " + comment;
+    }
 }
